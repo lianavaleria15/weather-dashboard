32a7de26783cb6e476d441fceac53990d1ea4c36
liana valeria <liana.valeria15@gmail.com>
2021-10-15 17:50:14 +0100

    added flexbox properties and API key

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,15 @@
-console.log("hello");
+// variable that stores the API key
+const APIKey = "71382d9165047b8c2f705fcf08ff20ed";
+
+//variable that collect user's input and stores it
+const city = "London";
+
+//construct a query URL to make the API call
+const queryURL =
+  "http://api.openweathermap.org/data/2.5/weather?q=" +
+  city +
+  "&appid=" +
+  APIKey;
+
+//make the API call using fetch
+fetch(queryURL);
