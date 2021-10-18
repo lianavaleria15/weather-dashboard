5e168c86837debc632ac417ca587c906b803a34f
liana valeria <liana.valeria15@gmail.com>
2021-10-18 14:31:08 +0100

    added event on submit button, added function to store city to local storage and get from local storage

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,40 +1,81 @@
+//target submit button on search container
+const submitBtn = $("#search-button");
+
+// function to add city to local storage
+const updateLocalStorage = (city) => {
+  //get city from local storage
+  const citiesSearched = JSON.parse(localStorage.getItem("cities")) ?? [];
+
+  //push new search to local storage
+  citiesSearched.push(city);
+
+  //add new city to local storage
+  localStorage.setItem("cities", JSON.stringify(citiesSearched));
+
+  return citiesSearched;
+};
+
+//function to render search history
+const renderSearchHistory = () => {};
+
+//get text from input
+const getCityName = (event) => {
+  event.preventDefault;
+
+  //get value from search input and assign to a variable
+  const city = $("#city-input").val();
+
+  //validate if input is empty
+  if (city) {
+    //update local storage with ciy value
+    updateLocalStorage(city);
+
+    //re-render search history
+  } else {
+    alert("No city added");
+  }
+};
+
+//add event on submit button
+submitBtn.on("click", getCityName);
+
 // variable that stores the API key
-const APIKey = "e44640c3292c7704425b7a92efe4de75";
+// const APIKey = "e44640c3292c7704425b7a92efe4de75";
 
 //variable that collect user's input and stores it
-const city = "London";
+// const city = "London";
 
 //construct a query URL to make the API call
-const queryURL =
-  "http://api.openweathermap.org/data/2.5/weather?q=" +
-  city +
-  "&appid=" +
-  APIKey;
+// const queryURL =
+//   "http://api.openweathermap.org/data/2.5/weather?q=" +
+//   city +
+//   "&appid=" +
+//   APIKey;
 
-//render today's city weather
-const renderCityWeather = () => {
-  console.log("Hooray");
-};
+// //render today's city weather
+// const renderCityWeather = () => {
+//   console.log("Hooray");
+// };
 
-const onLoad = () => {
-  //handle API response
-  const handleResponse = (response) => {
-    if (response.status !== 200) {
-      throw new Error("Something went wrong");
-    }
-    return response.json();
-  };
+// const onLoad = () => {
+//   //handle API response
+//   const handleResponse = (response) => {
+//     if (response.status !== 200) {
+//       throw new Error("Something went wrong");
+//     }
+//     return response.json();
+//   };
 
-  //handle error function
-  const handleError = (error) => {
-    console.log(error);
-  };
+//   //handle error function
+//   const handleError = (error) => {
+//     console.log(error);
+//   };
 
-  //get data from the API
-  fetch(queryURL)
-    .then(handleResponse)
-    .then(renderCityWeather)
-    .catch(handleError);
-};
-
+//   //get data from the API
+//   fetch(queryURL)
+//     .then(handleResponse)
+//     .then(renderCityWeather)
+//     .catch(handleError);
+// };
+const onLoad = () => {};
 $(window).on("load", onLoad);
