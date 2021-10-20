c760568aa166ee309e6f0eb909cb8e7a44a2aa48
liana valeria <liana.valeria15@gmail.com>
2021-10-20 20:57:46 +0100

    deleted existing code and added mock data to build render weather cards function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,93 +1,49 @@
-//target submit button on search container
-const submitBtn = $("#search-button");
-
-//target city list container
-const cityContainer = $("#city-list");
-
-// variable that stores the API key
-const APIKey = "e44640c3292c7704425b7a92efe4de75";
-
-// function to add city to local storage
-const updateLocalStorage = (city) => {
-  //get city from local storage
-  const citiesSearched = JSON.parse(localStorage.getItem("cities")) ?? [];
-
-  //push new search to local storage
-  citiesSearched.push(city);
-
-  //add new city to local storage
-  localStorage.setItem("cities", JSON.stringify(citiesSearched));
-
-  return citiesSearched;
-};
-
-//function to render search history
-const renderSearchHistory = (citiesSearched) => {
-  //build city item
-  const constructCityListItem = (city) => {
-    const cityListItem = `<li class="list-group-item">${city}</li>`;
-
-    cityContainer.append(cityListItem);
-  };
-  //append city item to list container
-  citiesSearched.forEach(constructCityListItem);
-};
-
-//get text from input
-const getCityName = (event) => {
-  event.preventDefault;
-
-  //get value from search input and assign to a variable
-  const city = $("#city-input").val();
-
-  //validate if input is empty
-  if (city) {
-    //construct a query URL to make the API call
-    const queryURL =
-      "http://api.openweathermap.org/data/2.5/weather?q=" +
-      city +
-      "&appid=" +
-      APIKey;
-
-    //get data from the API
-    fetch(queryURL)
-      .then(handleResponse)
-      .then(renderCurrentWeather)
-      .catch(handleError);
-
-    //update local storage with ciy value
-    updateLocalStorage(city);
-
-    //render search history
-    renderSearchHistory();
-
-    //re-render search history
-  } else {
-    alert("No city added");
-  }
-};
-
-//add event on submit button
-submitBtn.on("click", getCityName);
-
-// //render today's city weather
-// const renderCityWeather = () => {
-//   console.log("Hooray");
-// };
-
-// const onLoad = () => {
-//   //handle API response
-//   const handleResponse = (response) => {
-//     if (response.status !== 200) {
-//       throw new Error("Something went wrong");
-//     }
-//     return response.json();
-//   };
-
-//   //handle error function
-//   const handleError = (error) => {
-//     console.log(error);
-//   };
-
-const onLoad = () => {};
-$(window).on("load", onLoad);
+// use mock data here to construct and render weather containers before getting api data
+const weatherData ={
+  current: {
+    name: "London",
+    temperature: 123.45,
+    wind: 111.22,
+    humidity: 33,
+    uvi: 2.5,
+    date: "(3/30/2021)"
+    iconCode: "04n"
+  },
+  forecast: [
+    {
+      date: "(3/30/2021)",
+      temperature: 123.45,
+      wind: 111.22,
+      humidity: 33,
+      iconCode: "04n"
+    },
+    {
+      date: "(3/30/2021)",
+      temperature: 123.45,
+      wind: 111.22,
+      humidity: 33,
+      iconCode: "04n"
+    },
+    {
+      date: "(3/30/2021)",
+      temperature: 123.45,
+      wind: 111.22,
+      humidity: 33,
+      iconCode: "04n"
+    },
+    {
+      date: "(3/30/2021)",
+      temperature: 123.45,
+      wind: 111.22,
+      humidity: 33,
+      iconCode: "04n"
+    },
+    {
+      date: "(3/30/2021)",
+      temperature: 123.45,
+      wind: 111.22,
+      humidity: 33,
+      iconCode: "04n"
+    }
+  ]
+}
