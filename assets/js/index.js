e4513fef8fa6ab8e0fcf8b0d4d512b381d990f85
liana valeria <liana.valeria15@gmail.com>
2021-10-20 22:34:29 +0100

    added construct forecast weather cards container function and refactored append order

--- assets/js/index.js
+++ assets/js/index.js
@@ -48,25 +48,65 @@ const weatherData = {
   ],
 };
 
-//target weather card container
-const currentWeatherContainer = $("#current-weather-container");
-//construct current weather card function
+//target current weather card container
+const weatherCardsContainer = $("#weather-cards-container");
+
+//construct current weather card
 const constructCurrentWeatherCard = (data) => {
   //use template string to construct current weather card
   const currentWeatherCard = `<div class="card-body bg-white border mb-2">
     <h2 class="card-title">
-      ${data.current.name} ${data.current.date}
-      <img src="http://openweathermap.org/img/w/${data.current.iconCode}.png" />
+      ${data.name} ${data.date}
+      <img src="http://openweathermap.org/img/w/${data.iconCode}.png" />
     </h2>
-    <p class="card-text">Temp: ${data.current.temperature} &deg;F</p>
-    <p class="card-text">Wind: ${data.current.wind} MPH</p>
-    <p class="card-text">Humidity: ${data.current.humidity} %</p>
+    <p class="card-text">Temp: ${data.temperature} &deg;F</p>
+    <p class="card-text">Wind: ${data.wind} MPH</p>
+    <p class="card-text">Humidity: ${data.humidity} %</p>
     <p class="card-text">
-      UV index: <span class="btn btn-primary">${data.current.uvi}</span>
+      UV index: <span class="btn btn-primary">${data.uvi}</span>
     </p>
   </div>`;
-  currentWeatherContainer.append(currentWeatherCard);
+  weatherCardsContainer.append(currentWeatherCard);
+};
+
+//construct forecast weather cards
+const constructForecastWeatherCards = (forecastData) => {
+  //callback function to construct each card
+  const constructAndAppendForecastWeatherCard = (each) => {
+    return `<div class="card m-1" style="width: 18rem">
+      <div class="card-body">
+        <h5 class="card-title">${each.date}</h5>
+        <p class="card-text">
+          <img src="http://openweathermap.org/img/w/04n.png" />
+        </p>
+        <p class="card-text">Temp: 45.67 &deg;F</p>
+        <p class="card-text">Wind: 15.43 MPH</p>
+        <p class="card-text">Humidity: 78%</p>
+      </div>
+    </div>
+  `;
+  };
+
+  //construct forecast card for each date
+  const forecastCards = forecastData
+    .map(constructAndAppendForecastWeatherCard)
+    .join("");
+
+  //construct forecast weather container
+  const forecastWeatherContainer = `<div class="bg-white border">
+    <h3 class="p-3 text-center">5-Day Forecast:</h3>
+    <!--container forecast weather cards-->
+    <div
+      class=" m-3 d-flex flex-wrap justify-content-around"
+    ><div
+    class="d-flex flex-wrap justify-content-around"
+    id="forecast-weather-container"
+  >${forecastCards}</div></div>`;
+
+  //append forecast weather container to main weather container
+  weatherCardsContainer.append(forecastWeatherContainer);
 };
 
 //render weather containers
-constructCurrentWeatherCard(weatherData);
+constructCurrentWeatherCard(weatherData.current);
+constructForecastWeatherCards(weatherData.forecast);
